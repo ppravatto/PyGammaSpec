cc5286e6ae5179382be8f937b7cea998327fc216
ppravatto <pravatto1994@gmail.com>
2023-12-28 22:00:50 +0100

    Added option to calibrate the gamma spectrum from channel to energy

--- src/pygammaspec/visualization.py
+++ src/pygammaspec/visualization.py
@@ -1,10 +1,18 @@
 import matplotlib.pyplot as plt
-from typing import Optional
+from typing import Optional, List
 
 from pygammaspec.spectrum import GammaSpectrum
 from pygammaspec.analysis import Calibration
 
-def plot_spectrum(sample: GammaSpectrum, background: GammaSpectrum, xlog: bool = False, ylog: bool = False, filename: Optional[str] = None):
+
+def plot_spectrum(
+    sample: GammaSpectrum,
+    background: GammaSpectrum,
+    xlog: bool = False,
+    ylog: bool = False,
+    filename: Optional[str] = None,
+    calibration: Optional[Calibration] = None,
+):
     """
     The function plots the gamma spectrum of the sample together with that of the background and automatically computes the
     difference of the two spectra.
@@ -21,36 +29,64 @@ def plot_spectrum(sample: GammaSpectrum, background: GammaSpectrum, xlog: bool =
         If set to True will set the y-scale to logarithmic form.
     filename: Optional[str]
         If not None, will indicate the path of the generated image file.
+    calibration: Optional[Calibration]
+        If not None, will apply energy scale to the x-axis
     """
 
     plt.rc("font", **{"size": 14})
 
     fig, (ax1, ax2) = plt.subplots(nrows=2, figsize=(12, 10))
 
-    ax1.plot(background.spectrum[0], background.spectrum[1], c='#00AAAA', label="Background")
-    ax1.plot(sample.spectrum[0], sample.spectrum[1], c="black", label="Sample")
+    ax1.plot(
+        [calibration.get_energy(c) if calibration else c for c in background.channels],
+        background.counts,
+        c="#00AAAA",
+        label="Background",
+    )
+    ax1.plot(
+        [calibration.get_energy(c) if calibration else c for c in sample.channels],
+        sample.counts,
+        c="black",
+        label="Sample",
+    )
 
     ax1.legend()
     ax1.set_ylabel("Activity (cps)", size=16)
 
     difference = sample - background
-    averaged_difference = difference.average_smoothing(10)
+    avg_difference = difference.average_smoothing(10)
 
-    ax2.plot(difference.spectrum[0], difference.spectrum[1], c='#AAAAAA')
-    ax2.plot(averaged_difference.spectrum[0], averaged_difference.spectrum[1], c="black")
+    ax2.plot(
+        [calibration.get_energy(c) if calibration else c for c in difference.channels],
+        difference.counts,
+        c="#AAAAAA",
+    )
+    ax2.plot(
+        [calibration.get_energy(c) if calibration else c for c in avg_difference.channels],
+        avg_difference.counts,
+        c="black",
+    )
 
-    #ax2.set_xlim(plot_range)
-    ax2.set_xlabel("Channel", size=16)
+    # ax2.set_xlim(plot_range)
     ax2.set_ylabel("Activity (cps)", size=16)
+
+    if calibration:
+        ax2.set_xlabel("Energy (keV)", size=16)
+    else:
+        ax2.set_xlabel("Channel", size=16)    
 
     if xlog:
         ax1.set_xscale("log")
         ax2.set_xscale("log")
-    
+
     if ylog:
         ax1.set_yscale("log")
         ax2.set_yscale("log")
-        ax2.set_ylim(bottom=min([x if x>0 else max(difference.counts) for x in difference.counts ]))
+        ax2.set_ylim(
+            bottom=min(
+                [x if x > 0 else max(difference.counts) for x in difference.counts]
+            )
+        )
 
     ax1.grid(which="major", c="#DDDDDD")
     ax1.grid(which="minor", c="#EEEEEE")
@@ -61,7 +97,7 @@ def plot_spectrum(sample: GammaSpectrum, background: GammaSpectrum, xlog: bool =
 
     if filename is not None:
         plt.savefig(filename, dpi=600)
-    
+
     plt.show()
 
 
@@ -82,7 +118,10 @@ def plot_calibration(calibraton: Calibration) -> None:
 
     xfit, yfit = [], []
     for i in range(1000):
-        x = 0.8*min(calibraton.channels) + 1.4*i*(max(calibraton.channels) - min(calibraton.channels))/1000
+        x = (
+            0.8 * min(calibraton.channels)
+            + 1.4 * i * (max(calibraton.channels) - min(calibraton.channels)) / 1000
+        )
         y = calibraton.get_energy(x)
 
         xfit.append(x)
