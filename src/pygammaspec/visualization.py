82fb40695eaad22b4ac80a352a7e8ba631bc3dd7
ppravatto <pravatto1994@gmail.com>
2023-12-28 14:05:53 +0100

    Added calibration class + calibration plotter

--- src/pygammaspec/visualization.py
+++ src/pygammaspec/visualization.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 from typing import Optional
 
 from pygammaspec.spectrum import GammaSpectrum
+from pygammaspec.analysis import Calibration
 
 def plot_spectrum(sample: GammaSpectrum, background: GammaSpectrum, xlog: bool = False, ylog: bool = False, filename: Optional[str] = None):
     """
@@ -21,13 +22,16 @@ def plot_spectrum(sample: GammaSpectrum, background: GammaSpectrum, xlog: bool =
     filename: Optional[str]
         If not None, will indicate the path of the generated image file.
     """
-    fig, (ax1, ax2) = plt.subplots(nrows=2, figsize=(16, 10))
+
+    plt.rc("font", **{"size": 14})
+
+    fig, (ax1, ax2) = plt.subplots(nrows=2, figsize=(12, 10))
 
     ax1.plot(background.spectrum[0], background.spectrum[1], c='#00AAAA', label="Background")
     ax1.plot(sample.spectrum[0], sample.spectrum[1], c="black", label="Sample")
 
     ax1.legend()
-    ax1.set_ylabel("cps")
+    ax1.set_ylabel("Activity (cps)", size=16)
 
     difference = sample - background
     averaged_difference = difference.average_smoothing(10)
@@ -36,8 +40,8 @@ def plot_spectrum(sample: GammaSpectrum, background: GammaSpectrum, xlog: bool =
     ax2.plot(averaged_difference.spectrum[0], averaged_difference.spectrum[1], c="black")
 
     #ax2.set_xlim(plot_range)
-    ax2.set_xlabel("Bins")
-    ax2.set_ylabel("cps")
+    ax2.set_xlabel("Channel", size=16)
+    ax2.set_ylabel("Activity (cps)", size=16)
 
     if xlog:
         ax1.set_xscale("log")
@@ -48,9 +52,49 @@ def plot_spectrum(sample: GammaSpectrum, background: GammaSpectrum, xlog: bool =
         ax2.set_yscale("log")
         ax2.set_ylim(bottom=min([x if x>0 else max(difference.counts) for x in difference.counts ]))
 
+    ax1.grid(which="major", c="#DDDDDD")
+    ax1.grid(which="minor", c="#EEEEEE")
+    ax2.grid(which="major", c="#DDDDDD")
+    ax2.grid(which="minor", c="#EEEEEE")
+
     plt.tight_layout()
 
     if filename is not None:
         plt.savefig(filename, dpi=600)
     
+    plt.show()
+
+
+def plot_calibration(calibraton: Calibration) -> None:
+    """
+    Plot the calibration points together with the fitting curve.
+
+    Arguments
+    ---------
+    calibration: Calibration
+        The calibration object to plot.
+    """
+    plt.rc("font", **{"size": 14})
+
+    fig = plt.figure(figsize=(6, 6))
+
+    plt.scatter(calibraton.channels, calibraton.energies, marker="+", s=150, c="red")
+
+    xfit, yfit = [], []
+    for i in range(1000):
+        x = 0.8*min(calibraton.channels) + 1.4*i*(max(calibraton.channels) - min(calibraton.channels))/1000
+        y = calibraton.get_energy(x)
+
+        xfit.append(x)
+        yfit.append(y)
+
+    plt.plot(xfit, yfit, c="black", linestyle="--")
+
+    plt.xlabel("Channel", size=16)
+    plt.ylabel("Energy (keV)", size=16)
+
+    plt.grid(which="major", c="#DDDDDD")
+    plt.grid(which="minor", c="#EEEEEE")
+
+    plt.tight_layout()
     plt.show()
